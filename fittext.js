f4fb6ae170563229603a16356686d72fc28b6d43
Niklas Rasmusson <niklas.rasmusson@gmail.com>
2013-10-18 10:31:49 +0200

    Angularized FitText

--- fittext.js
+++ fittext.js
@@ -1,62 +1,37 @@
-/*!	
-* FitText.js 1.0 jQuery free version
+/*!
+* FitText.js 1.0 AngularJS version
 *
-* Copyright 2011, Dave Rupert http://daverupert.com 
-* Released under the WTFPL license 
+* Copyright 2011, Dave Rupert http://daverupert.com
+* Released under the WTFPL license
 * http://sam.zoy.org/wtfpl/
 * Modified by Slawomir Kolodziej http://slawekk.info
+* Angularized by Niklas Rasmusson http://rasmusson.net
 *
 * Date: Tue Aug 09 2011 10:45:54 GMT+0200 (CEST)
 */
-(function(){
-  var css = function (el, prop) {
-    return window.getComputedStyle ? getComputedStyle(el).getPropertyValue(prop) : el.currentStyle[prop];
-  };
-  
-  var addEvent = function (el, type, fn) {
-    if (el.addEventListener)
-      el.addEventListener(type, fn, false);
-		else
-			el.attachEvent('on'+type, fn);
-  };
-  
-  var extend = function(obj,ext){
-    for(key in ext)
-      if(ext.hasOwnProperty(key))
-        obj[key] = ext[key];
-    return obj;
-  };
-
-  window.fitText = function (el, kompressor, options) {
-
-    var settings = extend({
-      'minFontSize' : -1/0,
-      'maxFontSize' : 1/0
-    },options);
-
-    var fit = function (el) {
-      var compressor = kompressor || 1;
+angular.module('fitText', [])
+.directive('fitText', ['$window', function ($window) {
+  return {
+    restrict: 'A',
+    link: function (scope, elem, attrs) {
+      var settings = {
+        'minFontSize' : attrs.minFontSize || -1/0,
+        'maxFontSize' : attrs.maxFontSize || 1/0,
+        'compressor'  : attrs.compressor || 1
+      };
 
       var resizer = function () {
-        el.style.fontSize = Math.max(Math.min(el.clientWidth / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)) + 'px';
+        elem.css('fontSize', Math.max(Math.min(elem[0].clientWidth / (parseFloat(settings.compressor)*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)) + 'px');
       };
 
       // Call once to set.
       resizer();
 
-      // Bind events
-      // If you have any js library which support Events, replace this part
-      // and remove addEvent function (or use original jQuery version)
-      addEvent(window, 'resize', resizer);
-    };
-
-    if (el.length)
-      for(var i=0; i<el.length; i++)
-        fit(el[i]);
-    else
-      fit(el);
-
-    // return set of elements
-    return el;
+      angular.element($window).bind('resize', function () {
+        scope.$apply(function () {
+          resizer();
+        });
+      });
+    }
   };
-})();
+}]);
